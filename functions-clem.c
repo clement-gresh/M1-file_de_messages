235083c96cef950e2f72e0e38e680493eb82a650
clement-gresh <clem_paname@hotmail.com>
2022-04-03 01:10:33 +0200

    fonctionnalite : ajout des declarations des fonctions et des structures dans m_file.h

--- functions-clem.c
+++ functions-clem.c
@@ -3,6 +3,10 @@
 
 #include "m_file.h"
 
+int m_envoi(MESSAGE *file, const void *msg, size_t len, int msgflag){
+	return 0; // debug : ligne a changer
+}
+
 
 int main(int argc, const char * argv[]) {
 
--- m_file.h
+++ m_file.h
@@ -1,9 +1,42 @@
 #ifndef M_FILE_H_
 #define M_FILE_H_
 
+// STRUCTURES
 typedef struct MESSAGE{
 	enum{ RDWR,  RDONLY, WRONLY} flag;
 	void* memory;
 } MESSAGE;
+
+typedef struct mon_message{
+	long type;
+	char mtext[];
+} mon_message;
+
+typedef struct header{
+	int max_length_message;
+	int pipe_capacity;
+	int first;
+	int last;
+	//semaphore ou mutex
+} header;
+
+/*
+typedef struct pipe{
+	struct header head;
+	mon_message messages[]; // flexible array members are invalid
+} pipe;
+*/
+
+// FUNCTIONS
+MESSAGE *m_connexion( const char *nom, int options, size_t nb_msg, size_t len_max, mode_t mode );
+int m_deconnexion(MESSAGE *file);
+int m_destruction(const char *nom);
+
+int m_envoi(MESSAGE *file, const void *msg, size_t len, int msgflag);
+ssize_t m_reception(MESSAGE *file, void *msg, size_t len, long type, int flags);
+
+size_t m_message_len(MESSAGE *);
+size_t m_capacite(MESSAGE *);
+size_t m_nb(MESSAGE *);
 
 #endif /* M_FILE_H_ */
