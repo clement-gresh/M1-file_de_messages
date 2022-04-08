0535f3e19454c7ea6f0c9552ee6cf28004cf94c4
clement-gresh <clem_paname@hotmail.com>
2022-04-08 15:29:42 +0200

    fonctionnalite : creation de la fonction m_envoi avec verifications de certains de ses paramètres

--- functions-clem.c
+++ functions-clem.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
 
 #include "m_file.h"
 
+// <>
+
 int m_envoi(MESSAGE *file, const void *msg, size_t len, int msgflag){
+	if(file->flag == RDONLY){
+		printf("Impossible d'ecrire dans cette file.\n");
+		errno = EPERM;
+		exit(-1);
+	}
+	if(len > file->memory->max_length_message){
+		printf("La taille du message excede la taille maximale autorisee.\n");
+		errno = EMSGSIZE;
+		exit(-1);
+	}
+	if(msgflag != 0 && msgflag != O_NONBLOCK){
+		printf("Valeur de msgflag incorrecte.\n");
+		errno = EIO;
+		exit(-1);
+	}
+	if(file->memory->first == file->memory->last && (msgflag & O_NONBLOCK)){
+		printf("Le tableau est plein (envoi en mode non bloquant).\n");
+		errno = EAGAIN;
+		exit(-1);
+	}
 	return 0; // debug : ligne a changer
 }
 
