932f504534995f40762b2e6118b6d4849bccb2d1
hugolignier <hugolignier@penguin>
2022-04-19 15:46:00 +0200

    fonctionnalite: m_connexion premier test reussi

--- project-tests.c
+++ project-tests.c
@@ -2,10 +2,9 @@
 
 
 int main(int argc, const char * argv[]) {
-	/*
+	
 	MESSAGE* file = m_connexion("/kangourou", O_RDWR | O_CREAT, 12, sizeof(char)*50, S_IRWXU | S_IRWXG | S_IRWXO);
-	printf(&file->name);
-	*/
+	printf(file->name);
 
 	return EXIT_SUCCESS;
 }
